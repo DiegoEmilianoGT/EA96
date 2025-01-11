b16850666a8e24ee1e41f33a21bbe90d2f2bc8de
DiegoEmilianoGT <masclavk@gmail.com>
2025-01-10 21:03:11 -0600

    mi primer cambio

--- vocal.c
+++ vocal.c
@@ -8,23 +8,26 @@
 
 char phrase [100];
 
+int isVocal(int c) {
+    if(c=='a' ||c=='e'||c=='i'||c=='o'||c=='u'){
+        return 1;
+    }
+}
+
 int countVowels(char* phrase) {
-    int vocales = 0;
-    int i=strlen(phrase);
+    int vocales = 0, i=strlen(phrase);
     for(int y=0;y<i;y++){
-        if(phrase[y]=='a' ||phrase[y]=='e'||phrase[y]=='i'||phrase[y]=='o'||phrase[y]=='u'){
+       if (isVocal(phrase[i]))
             vocales++;
-        }
     }
     printf("\nEl numero de VOCALES que tiene la cadena es: %d\n", vocales);
     return vocales;
 }
 
-
 int countConsonants(char* phrase) {
     int count=0, i=strlen(phrase);
     while (phrase[i]){
-        if (!countVowels(i)) {
+        if (!isVocal(phrase[i])) {
             count++;
         }
         i++;
@@ -33,20 +36,14 @@ int countConsonants(char* phrase) {
     return count;
 }
 
-
-
-void getInput(char* ){
-    do {
+void getInput(){
         printf("Introduce una cadena: ");
         scanf("%s", phrase);
-    }while(strlen(phrase)>0);
-
 }
 
 int main(){
     getInput(phrase);
     countVowels(phrase);
     countConsonants(phrase);
-
     return 0;
 }
